333f711d59fd101081286b5070843d71d5e49c83
Abhip1423p <55909869+Abhip1423p@users.noreply.github.com>
2020-12-29 10:24:39 +0530

    all page

---
+++ app/src/main/java/com/example/vendor_app/garbageCollection.java
@@ -0,0 +1,14 @@
+package com.example.vendor_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class garbageCollection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_garbage_collection);
+    }
+}
